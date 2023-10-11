24dcd5e86f8d89c7f5589ae815c3f1661e5dce48
Алина Попадченко <popadchenko.a@gmail.com>
2023-10-11 23:34:23 +0300

    d3

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv  # type: ignore
-
+from dotenv import load_dotenv 
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
