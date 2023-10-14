579cc3aed2159a4f7a69380c7208fb144a519dd0
Алина Попадченко <popadchenko.a@gmail.com>
2023-10-14 11:36:09 +0300

    setings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', 'dbf'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
