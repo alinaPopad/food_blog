e4ac97956cbbfa6ad3bb6550760836c231f62274
Алина Попадченко <popadchenko.a@gmail.com>
2023-10-16 12:40:39 +0300

    f

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -257,21 +257,16 @@ class MiniRecipeSerializer(serializers.ModelSerializer):
 
 class FollowSerializer(serializers.ModelSerializer):
     """Сериализатор для подписки на авторов."""
-    email = serializers.EmailField(source='user.email')
-    username = serializers.CharField(source='user.username')
-    first_name = serializers.CharField(source='user.first_name')
-    last_name = serializers.CharField(source='user.last_name')
-
     is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField(
         read_only=True,
         method_name='get_recipes')
     recipes_count = serializers.SerializerMethodField(
         read_only=True
-    )  # comment
+    )
 
     class Meta:
-        model = Follow
+        model = CustomUser
         fields = ('email', 'id', 'username', 'first_name', 'last_name',
                   'is_subscribed', 'recipes', 'recipes_count')
 
@@ -281,9 +276,9 @@ class FollowSerializer(serializers.ModelSerializer):
             return False
         return Follow.objects.filter(user=user, author=obj.id).exists()
 
-    def get_recipes(self, obj):
+    def get_recipes(self, obj): 
         request = self.context.get('request')
-        recipes = Recipe.objects.filter(author=obj.author)
+        recipes = obj.recipes.all()
         recipes_limit = request.query_params.get('recipes_limit')
         if recipes_limit:
             recipes = recipes[:int(recipes_limit)]
