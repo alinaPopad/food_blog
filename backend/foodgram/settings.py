1bd7c7ca4b7bd728a75dc5cd2aca12cfe4a2de60
Алина Попадченко <popadchenko.a@gmail.com>
2023-10-19 00:12:37 +0300

    rev1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', default=False) == 'True'
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', default=False) == 'True'
+
 
 ALLOWED_HOSTS = [
     'taski2023.ddns.net',
